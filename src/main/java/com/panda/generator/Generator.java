57551f92215298c2bda90ef98da85ef4107cbf72
Rainpan <Rainpan@users.noreply.github.com>
2020-04-25 20:22:47 +0800

    Update Generator.java

--- src/main/java/com/panda/generator/Generator.java
+++ src/main/java/com/panda/generator/Generator.java
@@ -1,4 +1,15 @@
 package com.panda.generator;
 
 public class Generator {
+
+    public static void main( String[] args ) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        File configFile = new File("src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
 }
