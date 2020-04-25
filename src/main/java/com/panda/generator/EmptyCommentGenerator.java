d40f324ad35156188ed99ba24a603a30cca8cfb8
Rainpan <Rainpan@users.noreply.github.com>
2020-04-25 20:24:29 +0800

    Update EmptyCommentGenerator.java

--- src/main/java/com/panda/generator/EmptyCommentGenerator.java
+++ src/main/java/com/panda/generator/EmptyCommentGenerator.java
@@ -1,4 +1,94 @@
 package com.panda.generator;
 
-public class EmptyCommentGenerator {
+public class EmptyCommentGenerator implements CommentGenerator {
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
+
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
 }
