19eecaed9431ef2bb4f49946358c8cb05ed0de23
Rainpan <Rainpan@users.noreply.github.com>
2020-04-25 20:23:12 +0800

    Update MyJavaTypeResolverDefaultImpl.java

--- src/main/java/com/panda/generator/MyJavaTypeResolverDefaultImpl.java
+++ src/main/java/com/panda/generator/MyJavaTypeResolverDefaultImpl.java
@@ -1,4 +1,9 @@
 package com.panda.generator;
 
-public class MyJavaTypeResolverDefaultImpl {
+public class MyJavaTypeResolverDefaultImpl extends JavaTypeResolverDefaultImpl {
+
+    public MyJavaTypeResolverDefaultImpl(){
+        super();
+        super.typeMap.put(Types.TINYINT,new org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl.JdbcTypeInformation("TINYINT",new FullyQualifiedJavaType(Integer.class.getName())));
+    }
 }
